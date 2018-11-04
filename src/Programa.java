dbfb069a7a115a0febbbba7716dc225f2f1ac735
Artiom Samofalov <pavyzdinisVartotojas@gmail.com>
2018-11-04 20:04:18 +0200

    Pakeitimai programoje (antras commit)

--- src/Programa.java
+++ src/Programa.java
@@ -1,6 +1,8 @@
 
 public class Programa {
 	public static void main(String[] args) {
-		System.out.println("Cia yra pavyzdine programa, skirta git prezentacijai.");
+		System.out.println("Cia yra pavyzdine programa, skirta git prezentacijai. "); // Sioje eiluteje atsirado pakeitimu
 	}
+	
+	PapildomaKlase papildoma = new PapildomaKlase(); // Si eilute naujai prideta
 }
